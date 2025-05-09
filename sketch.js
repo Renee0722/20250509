19f5b1a5a14d5f9ce4234f787683a01891cf80b6
Renee0722 <renee950722@gmail.com>
2025-05-09 10:33:35 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,31 +10,32 @@ function preload() {
   handPose = ml5.handPose({ flipped: true });
 }
 
-function mousePressed() {
-  console.log(hands);
+function setup() {
+  createCanvas(800, 600); // 建立畫布
+  video = createCapture(VIDEO); // 啟用相機
+  video.size(800, 600);
+  video.hide(); // 隱藏原始相機畫面
+
+  // 啟用 HandPose 並設定回呼函式
+  handPose.on("predict", gotHands);
 }
 
 function gotHands(results) {
-  hands = results;
+  hands = results; // 更新偵測到的手部資料
 }
 
-function setup() {
-  createCanvas(800, 600); // 建立畫布
-  background(220);
+function draw() {
+  image(video, 0, 0); // 顯示相機畫面
 
-  // 假設 hand.keypoints 是一個包含 21 個點的陣列，每個點是 {x, y}
-  let hand = {
-    keypoints: [
-      { x: 50, y: 50 }, { x: 100, y: 100 }, { x: 150, y: 150 }, { x: 200, y: 200 }, { x: 250, y: 250 }, // 0-4
-      { x: 300, y: 50 }, { x: 350, y: 100 }, { x: 400, y: 150 }, { x: 450, y: 200 },                   // 5-8
-      { x: 500, y: 50 }, { x: 550, y: 100 }, { x: 600, y: 150 }, { x: 650, y: 200 },                   // 9-12
-      { x: 700, y: 50 }, { x: 750, y: 100 }, { x: 800, y: 150 }, { x: 850, y: 200 },                   // 13-16
-      { x: 900, y: 50 }, { x: 950, y: 100 }, { x: 1000, y: 150 }, { x: 1050, y: 200 }                  // 17-20
-    ]
-  };
-
-  // 繪製手部的線條
-  drawHandLines(hand);
+  // 確保至少偵測到一隻手
+  if (hands.length > 0) {
+    for (let hand of hands) {
+      if (hand.confidence > 0.1) {
+        // 繪製手部的線條
+        drawHandLines(hand);
+      }
+    }
+  }
 }
 
 function drawHandLines(hand) {
@@ -50,35 +51,22 @@ function drawHandLines(hand) {
   // 遍歷每個分組，畫出線條
   for (let group of groups) {
     for (let i = 0; i < group.length - 1; i++) {
-      let start = hand.keypoints[group[i]];
-      let end = hand.keypoints[group[i + 1]];
-      line(start.x, start.y, end.x, end.y); // 使用 p5.js 的 line 函式
-    }
-  }
-}
+      let start = hand.annotations[group[i]];
+      let end = hand.annotations[group[i + 1]];
 
-function draw() {
-  image(video, 0, 0);
-
-  // Ensure at least one hand is detected
-  if (hands.length > 0) {
-    for (let hand of hands) {
-      if (hand.confidence > 0.1) {
-        // Loop through keypoints and draw circles
-        for (let i = 0; i < hand.keypoints.length; i++) {
-          let keypoint = hand.keypoints[i];
-
-          // Color-code based on left or right hand
-          if (hand.handedness == "Left") {
-            fill(255, 0, 255);
-          } else {
-            fill(255, 255, 0);
-          }
-
-          noStroke();
-          circle(keypoint.x, keypoint.y, 16);
-        }
+      if (start && end) {
+        stroke(0, 255, 0); // 設定線條顏色
+        strokeWeight(2);   // 設定線條粗細
+        line(start[0][0], start[0][1], end[0][0], end[0][1]); // 畫線
       }
     }
+  }
+
+  // 繪製每個關鍵點
+  for (let i = 0; i < hand.landmarks.length; i++) {
+    let keypoint = hand.landmarks[i];
+    fill(255, 0, 0); // 設定點的顏色
+    noStroke();
+    circle(keypoint[0], keypoint[1], 10); // 繪製圓點
   }
 }
